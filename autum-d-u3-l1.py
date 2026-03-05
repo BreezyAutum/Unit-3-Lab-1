96019c8e3e54faf9c6a6a99529da0c1c55d9f4bb
Autum <autumdarrell@gmail.com>
2026-03-05 00:48:57 -0700

    Aside from the odd message in the terminal it work

--- autum-d-u3-l1.py
+++ autum-d-u3-l1.py
@@ -3,15 +3,28 @@ NUM_DICE_TO_ROLL = 5
 SEED = 2183
 
 # implement roll_dice() here! 
-import roll_dice
+def roll_dice():
+  '''Rolls dice and sets them to a list, so the full list can be displayed.'''
+  global dlist; dlist = []
+  NUM_DICE_TO_ROLL = int(input("How many dice?\n"))
+  for i in range(NUM_DICE_TO_ROLL):
+    roll = random.randint(1,6)
+    dlist.append(roll)
+  print(dlist)
+  return dlist, NUM_DICE_TO_ROLL, roll
+roll_dice()
 
 # implement most_repeats() here! 
-import repeats
+def repeats():
+    '''Finds how many times the most common number was rolled in roll_dice'''
+    repeats = roll_dice.dlist.count
+    repeats = int(repeats)
+    return repeats
  
 def main():
     random.seed(SEED) 
     # implement remaining logic here! 
-    print("Your roll of ", roll_dice.dlist, "contains ",repeats," of a kind.")
+    print("Your roll of", dlist, "contains", repeats," of a kind.")
     again = input("Do you want to roll again?\n")
     if "Y" in again:
         roll_dice()
